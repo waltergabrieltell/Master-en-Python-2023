511c89edf6062309b6ce512b08f51ce5fbd5cc48
Walter Tell <walterdjv@gmail.com>
2023-03-17 16:05:58 +0000

    Fin de clase Sqlite 17/03

--- 19-Bases de datos/sqlite2.py
+++ 19-Bases de datos/sqlite2.py
@@ -1,43 +1,43 @@
-# importar modulo de base de datos
+################################################ importar modulo de base de datos
 
 import sqlite3  # sqlite es una base de datos que trae python
 
-# Abrir la conexion
+################################################ Abrir la conexion
 
 conexion = sqlite3.connect("./19-Bases de datos/pruebas2.db")  # entre parentesis definimos el nombre para nuestra base de datos
 
-# Crear un cursos : es lo que me permite hacer consultas en una tabla
+################################################ Crear un cursos : es lo que me permite hacer consultas en una tabla
 
 cursor = conexion.cursor()
 
 
-# Crear una tabla
+################################################ Crear una tabla
 
 cursor.execute("""
 CREATE TABLE IF NOT EXISTS productos(
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    titulo VARCHAR(255),
-    descripcion TEXT,
-    precio int(255)
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Titulo VARCHAR(255),
+    Descripcion TEXT,
+    Precio int(255)
 );
 """)
 
-# Guardar cambios
+################################################ Guardar cambios
 
 conexion.commit()
 
-# Insertar datos
+################################################ Insertar datos
 
 """
 cursor.execute("INSERT INTO productos VALUES(null, 'Primer producto', 'Descripcion de mi producto', 550)")
 conexion.commit()
 """
 
-# Borrar registros 'todos los registros'
+################################################ Borrar registros 'todos los registros'
 
-# >> cursor.execute("DELETE FROM productos")
+cursor.execute("DELETE FROM productos")
 
-# Insertar muchos registros a la vez
+################################################ Insertar muchos registros a la vez
 
 productos = [
    ("Ordenador portatil", "Buen PC", 700),
@@ -46,12 +46,20 @@ productos = [
    ("Tablet", "Buena tablet", 300),
 ]
 cursor.executemany("INSERT INTO productos VALUES (null,?,?,?)", productos)
-cursor.commit()
+conexion.commit()
+
+################################################ Update »» Actualizar datos
+# si por ejemplo queremos que el precio de los productos que valen 80 se actualice a 678
+
+# cursor.execute("UPDATE productos SET precio = 678 WHERE precio = 80")
 
 
-# Listar Datos
+################################################ Listar Datos
 
 cursor.execute("SELECT * FROM productos;")
+# tambien puedo ponerle condiciones de que quiero que muestre
+#cursor.execute("SELECT * FROM productos WHERE precio >= 100;") # si queremos poner condicion para que nos muestre solo los produtos >= 100
+#cursor.execute("SELECT * FROM productos WHERE precio >= 300;") # si queremos poner condicion para que nos muestre solo los produtos >= 300
 productos = cursor.fetchall()
 
 print(f"\nEste es el cursor: {cursor}\n")
@@ -61,7 +69,7 @@ for producto in productos:
     print(f"Estos son los productos agregados: {producto}")
 
 for producto in productos:
-    print("")
+    print("ID: ", producto[0])
     print("Titulo: ", producto[1])
     print("Descripcion: ", producto[2])
     print("Precio: ", producto[3])
@@ -72,7 +80,7 @@ producto = cursor.fetchone()
 print(producto)
 
 
-# Cerrar la conexion
+################################################ Cerrar la conexion
 
 conexion.close()
 
